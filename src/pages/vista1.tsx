1318eb14a2a0f474480d260bc259950fce5d27c4
CiroAntony <vasquezciro654@gmail.com>
2024-10-24 23:21:44 -0500

    responsive y correcciones

--- src/pages/vista1.tsx
+++ src/pages/vista1.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function vista1() {
   return <div>vista1</div>;
 }
--- src/pages/vista2.tsx
+++ src/pages/vista2.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function vista2() {
   return <div>vista2</div>;
 }
